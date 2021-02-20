914b1587f478f2f97b8420fed041b614e6f868f7
Andrey Sitnik <andrey@sitnik.ru>
2021-02-20 12:13:02 -0500

    Add logux/subscribe and logux/unsubscribe

---
+++ subscriptions/index.d.ts
@@ -0,0 +1,38 @@
+import { ActionCreator } from '../define-action/index.js'
+
+export type LoguxSubscribeAction = {
+  type: 'logux/subscribe'
+  channel: string
+  filter?: {
+    [key: string]: string | number | boolean
+  }
+  since?: {
+    id: string
+    time: number
+  }
+}
+
+export type LoguxUnsubscribeAction = {
+  type: 'logux/unsubscribe'
+  channel: string
+  filter?: {
+    [key: string]: string | number | boolean
+  }
+}
+
+export const createLoguxSubscribe: ActionCreator<
+  LoguxSubscribeAction,
+  {
+    channel: string
+    filter?: LoguxSubscribeAction['filter']
+    since?: LoguxSubscribeAction['since']
+  }
+>
+
+export const createLoguxUnsubscribe: ActionCreator<
+  LoguxSubscribeAction,
+  {
+    channel: string
+    filter?: LoguxSubscribeAction['filter']
+  }
+>
---
+++ subscriptions/index.js
@@ -0,0 +1,5 @@
+import { defineAction } from '../define-action/index.js'
+
+export const createLoguxSubscribe = defineAction('logux/subscribe')
+
+export const createLoguxUnsubscribe = defineAction('logux/unsubscribe')
