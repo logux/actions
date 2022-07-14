e567f600e0b7267c1858f9fe554c293aa28d9154
Eduard Aksamitov <e@euaaaio.ru>
2022-07-15 00:09:47 +0300

    docs: fix and improve `LoguxNotFoundError` example

--- logux-not-found/index.d.ts
+++ logux-not-found/index.d.ts
@@ -2,13 +2,13 @@
  * An error for `load()` callback to return `logux/undo` with 404.
  *
  * ```js
- * import { LoguxNotFoundError } from '@logux/server'
+ * import { LoguxNotFoundError } from '@logux/actions'
  *
  * server.channel('posts/:id', {
- *   …
  *   load () {
  *     throw new LoguxNotFoundError()
- *   }
+ *   },
+ *   …
  * })
  * ```
  */
